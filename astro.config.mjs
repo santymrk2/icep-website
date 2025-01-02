d4a63d50eff92b17eb1b40d4ae9ca49236c2807a
victus-ubuntu-santy <santymc@icloud.com>
2025-01-02 20:01:09 +0000

    Add Activados and new menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  adapter: vercel()
 });
