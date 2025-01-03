62d5f2d29cab27ff483d71e5edc0ff3dcea86fd8
victus-ubuntu-santy <santymc@icloud.com>
2025-01-03 16:08:23 +0000

    Timeline edited colors and content

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -9,14 +9,15 @@ const Timeline = () => {
   return (
       <Chrono 
         mode="VERTICAL_ALTERNATING"  
+        class="text-black"
         items={items} 
         scrollable={{ scrollbar: true }}  
         theme={{
           primary: '#1e3a8a',
           secondary: '#1e3a8a',
           cardBgColor: 'white',
-          titleColor: 'black',
-          titleColorActive: 'white',
+          cardTitleColor: '#1e3a8a',
+
         }}
   />
   )
