38d5c6be02a5f561c1ed6e4e10fce07dba6f7612
victus-ubuntu-santy <santymc@icloud.com>
2025-01-14 02:32:24 +0000

    New Header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,9 +12,17 @@ export default {
 				sans: ['Poppins', ...defaultTheme.fontFamily.sans],
 			},
 		},
+		dropShadow: {
+			'3xl': '0 0 15px rgb(30, 58, 138)',
+			'4xl': [
+				'0 35px 35px rgba(0, 0, 0, 0.25)',
+				'0 45px 65px rgba(0, 0, 0, 0.15)'
+			]
+		  }
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
+		require('tailwindcss-motion'),
 	  ],
 
 }
