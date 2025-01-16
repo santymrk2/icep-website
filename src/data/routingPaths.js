8a7226de6abce303a6593a7e69bcc12c559bcf70
victus-ubuntu-santy <santymc@icloud.com>
2025-01-16 18:44:10 +0000

    New file for the Paths

---
+++ src/data/routingPaths.js
@@ -0,0 +1,55 @@
+const Paths = [
+    {
+      text: "Inicio",
+      href: "/",
+      subitems: undefined,
+    },
+    {
+      text: "Nosotros",
+      href: "/nosotros",
+      subitems: [
+        { text: "Historia", href: "/historia" },
+        { text: "Creencias", href: "/creencias" },
+        /*
+        { text: "Hermanos Responsables", href: "/responsables" },
+         */
+        { text: "Prólogo al libro", href: "/prologo" },
+        /*
+        {
+          text: "Actividades en orden cronologico",
+          href: "/actividades-cronologico",
+        },*/
+      ],
+    },
+    {
+      text: "Ministerios",
+      href: "/ministerios",
+      subitems: [
+        { text: "Niños", href: "/ninios" },
+        { text: "Adolescentes", href: "/adolescentes" },
+        { text: "Jovenes", href: "/jovenes" },
+        { text: "Mujeres", href: "/mujeres" },
+        { text: "Matrimonios", href: "/matrimonios" },
+      ],
+    },
+    /*
+    {
+      text: "Instituto Bíblico",
+      href: "/instituto",
+      subitems: undefined 
+    },
+    */
+    {
+      text: "Calendario",
+      href: "https://iglesia-pilar.notion.site/b19a403082ee49238154f16433dd7925?v=7851ff6a1d2c403da600451c9e99c129&pvs=74",
+      subitems: undefined,
+    },
+    {
+      text: "Contacto",
+      href: "/contacto",
+      subitems: undefined,
+    },
+  ];
+
+export default Paths
+  
