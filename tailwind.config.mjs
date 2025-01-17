bf19007c2ad2a8d9ae0a49321556e6c39a242988
victus-ubuntu-santy <santymc@icloud.com>
2025-01-17 00:44:23 +0000

    gradient header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+import { transformer } from 'astro:schema'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
@@ -13,6 +14,7 @@ export default {
 			},
 			boxShadow: {
 				'custom-blue': '0 0 25px rgba(42, 56, 178, 0.5)',
+		
 				'lg': '0 0 20px rgba(42, 56, 178, 0.1)',
 			},
 			borderRadius: {
@@ -20,6 +22,23 @@ export default {
 			},
 			colors: {
 				'custom-blue': '#2A38B2',
+				'custom-teal': '#0180f1',
+			},
+			animation: {
+				tilt: 'tilt 10s infinite linear'
+			},
+			keyframes:{
+				tilt: {
+					"0%, 50%, 100%": {
+						transform: "rotate(0deg)",
+					},
+					"25%": {
+						transform: "rotate(1deg)",
+					},
+					"75%": {
+						transform: "rotate(-1deg)"
+					}
+				}
 			}
 		},
 	},
