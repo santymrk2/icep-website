6624c243a7162fdae4c8b2a7dafab12dc3afe8ce
victus-ubuntu-santy <santymc@icloud.com>
2025-01-18 02:10:24 +0000

    Env variables activated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
@@ -10,4 +10,10 @@ export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
   adapter: vercel(),
+  env: {
+    schema: {
+      NOTION_API_KEY: envField.string({context:'server', access: 'secret'}),
+      DATABASE_ID: envField.string({context:'server', access: 'public' })
+    }
+  }
 });
