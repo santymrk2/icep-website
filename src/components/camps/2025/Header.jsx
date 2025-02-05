5e94e7a6067508136f6d830fb49fd92dfd0f3020
victus-ubuntu-santy <santymc@icloud.com>
2025-02-05 21:09:38 +0000

    New webkit mask for ios

--- src/components/camps/2025/Header.jsx
+++ src/components/camps/2025/Header.jsx
@@ -112,12 +112,12 @@ const Navbar = ({ location }) => {
                                                 className={`inline-flex ${location === item.href ? buttonSelectClasses : buttonClasses}`}
                                             >
                                                 {item.icon && (
-                                                    <div className={`w-6 h-6 mr-2 ${location === item.href ? "bg-green-900" : "bg-white"} mask`} style={{maskImage: `url(${item.icon})`}}>
+                                                    <div className={`w-6 h-6 mr-2 ${location === item.href ? "bg-green-900" : "bg-white"}`} style={{ WebkitMaskImage: `url(${item.icon})`, maskImage: `url(${item.icon})` }}
+                                                    >
                                                         <img
                                                             src={item.icon}
                                                             alt={item.text}
-                                                            className="w-full h-full object-contain"
-                                                            style={{opacity: 0}}
+                                                            className="w-full h-full object-contain opacity-0"
                                                         />
                                                     </div>
                                                 )}
@@ -227,16 +227,16 @@ const Navbar = ({ location }) => {
                                         href={item.href}
                                         className={`inline-flex justify-center w-full ${location === item.href ? buttonSelectClasses : buttonClasses}`}
                                     >
-                                                                                    {item.icon && (
-                                                    <div className={`w-6 h-6 mr-2 ${location === item.href ? "bg-green-900" : "bg-white"} mask`} style={{maskImage: `url(${item.icon})`}}>
-                                                        <img
-                                                            src={item.icon}
-                                                            alt={item.text}
-                                                            className="w-full h-full object-contain"
-                                                            style={{opacity: 0}}
-                                                        />
-                                                    </div>
-                                                )}
+                                        {item.icon && (
+                                            <div className={`w-6 h-6 mr-2 ${location === item.href ? "bg-green-900" : "bg-white"} `} style={{ WebkitMaskImage: `url(${item.icon})`, maskImage: `url(${item.icon})` }}
+                                            >
+                                                <img
+                                                    src={item.icon}
+                                                    alt={item.text}
+                                                    className="w-full h-full object-contain opacity-0"
+                                                />
+                                            </div>
+                                        )}
                                         {item.text}
                                     </a>
                                 ) : (
