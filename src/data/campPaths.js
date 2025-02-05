7ddcb317c741a14efd283188fcf1e1a5d0edbf48
victus-ubuntu-santy <santymc@icloud.com>
2025-02-05 22:09:39 +0000

    New try svg

--- src/data/campPaths.js
+++ src/data/campPaths.js
@@ -5,19 +5,19 @@ const Paths = [
       text: "Inicio",
       href: "/camps/2025/home",
       subitems: undefined,
-      icon: "../../public/home-icon.svg",
+      icon: "/home-icon.svg",
     },
     {
       text: "Alabanza",
       href: "/camps/2025/worship",
       subitems: undefined,
-      icon: "../../public/worship-icon.svg",
+      icon: "/worship-icon.svg",
     },
     {
       text: "Horario",
       href: "/camps/2025/schedule",
       subitems: undefined,
-      icon: "../../public/time-icon.svg",
+      icon: "/time-icon.svg",
     },
     {
       text: "Volver a ICEPilar",
