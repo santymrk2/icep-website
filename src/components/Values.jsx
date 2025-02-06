cbf6b341ba3bfa7c5ea133e0a3b2fb8ff22441b9
victus-ubuntu-santy <santymc@icloud.com>
2025-02-06 15:29:44 +0000

    Values

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -45,7 +45,7 @@ const ValuesCarousel = () => {
           {/* Flechas de navegación */}
           <button 
             onClick={() => handleScroll('left')}
-            className="absolute left-4 top-1/2 -translate-y-1/2 p-2 z-10"
+            className="absolute left-4 top-1/2 -translate-y-1/2 p-2 z-10 rounded-full bg-zinc-800/70"
           >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
@@ -54,7 +54,7 @@ const ValuesCarousel = () => {
           
           <button 
             onClick={() => handleScroll('right')}
-            className="absolute right-4 top-1/2 -translate-y-1/2 p-2 z-10"
+            className="absolute right-4 top-1/2 -translate-y-1/2 p-2 z-10 rounded-full bg-zinc-800/70"
           >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
