e06b4622acc0651433e0954a586202b638f842e3
santy-air <santymc@icloud.com>
2025-08-01 23:21:37 -0300

    Se agrego el env correspondiente al site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,28 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from '@astrojs/vercel';
-import sitemap from '@astrojs/sitemap';
-
+import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), sitemap()],
-  site: 'https://www.icepilar.org',
+  site: "https://www.icepilar.org",
 
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'server',
+  output: "server",
   adapter: vercel(),
   env: {
     schema: {
-      NOTION_API_KEY: envField.string({context:'server', access: 'secret'}),
-      DATABASE_ID: envField.string({context:'server', access: 'public' }),
-      GOOGLE_API_KEY: envField.string({context: 'server', access: 'secret'})
-    }
-  }
+      NOTION_API_KEY: envField.string({ context: "server", access: "secret" }),
+      DATABASE_ID: envField.string({ context: "server", access: "public" }),
+      GOOGLE_API_KEY: envField.string({ context: "server", access: "secret" }),
+      SITE: envField.string({ context: "server", access: "public" }),
+    },
+  },
 });
+
